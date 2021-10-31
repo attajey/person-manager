e369bbb622308b98f765db61ee18e218f3085397
Atta Jirofty <jirofty76@gmail.com>
2021-10-31 12:57:58 +0330

    useContext added to Persons.jsx

--- src/components/Person/Persons.jsx
+++ src/components/Person/Persons.jsx
@@ -1,23 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import SimpleContext from "../../context/SimpleContext";
 import Person from "./Person";
 
 const Persons = () => {
+  const context = useContext(SimpleContext);
+  const { handleDeletePerson, handleNameChange } = context;
+
   return (
-    <SimpleContext.Consumer>
-      {(context) => (
-        <div>
-          {context.state.persons.map((person) => (
-            <Person
-              key={person.id}
-              fullname={person.fullname}
-              deleted={() => context.handleDeletePerson(person.id)}
-              changed={(event) => context.handleNameChange(event, person.id)}
-            />
-          ))}
-        </div>
-      )}
-    </SimpleContext.Consumer>
+    // <SimpleContext.Consumer>
+    // {(context) => (
+    <div>
+      {context.state.persons.map((person) => (
+        <Person
+          key={person.id}
+          fullname={person.fullname}
+          deleted={() => handleDeletePerson(person.id)}
+          changed={(event) => handleNameChange(event, person.id)}
+        />
+      ))}
+    </div>
+    // )}
+    // </SimpleContext.Consumer>
   );
 };
 
