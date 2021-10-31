ba29211928346e99f89df298ce8a60d6f572283a
Atta Jirofty <jirofty76@gmail.com>
2021-10-31 12:50:06 +0330

    used useContext() in Header.js
    By using useContext() in Header.js now we can use badgestyles easily.
    Also destructed persons and appTitle from context.state for more readable code.

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,35 +1,39 @@
-import React from "react";
+import React, { useContext } from "react";
 import SimpleContext from "../../context/SimpleContext";
 
-const Header = ({ personsLength }) => {
-  let badgeStyle = [];
+const Header = () => {
+  const context = useContext(SimpleContext);
 
-  if (personsLength >= 3) {
-    badgeStyle.push("badge-success");
+  const { persons, appTitle } = context.state;
+
+  let badgeStyle = "";
+
+  if (persons.length >= 3) {
+    badgeStyle = "badge-success";
   }
-  if (personsLength <= 2) {
-    badgeStyle.push("badge-warning");
+  if (persons.length <= 2) {
+    badgeStyle = "badge-warning";
   }
-  if (personsLength <= 1) {
-    badgeStyle.push("badge-danger");
+  if (persons.length <= 1) {
+    badgeStyle = "badge-danger";
   }
 
   return (
-    <SimpleContext.Consumer>
-      {(context) => (
-        <div>
-          <div className="alert alert-info">
-            <h2>{context.state.appTitle}</h2>
-          </div>
-          <h5 className="alert alert-light">
-            Number of Persons :{" "}
-            <span className={`badge badge-pill ${badgeStyle.join(" ")}`}>
-              {context.state.persons.length}
-            </span>
-          </h5>
-        </div>
-      )}
-    </SimpleContext.Consumer>
+    // <SimpleContext.Consumer>
+    // {(context) => (
+    <div>
+      <div className="alert alert-info">
+        <h2>{appTitle}</h2>
+      </div>
+      <h5 className="alert alert-light">
+        Number of Persons :{" "}
+        <span className={`badge badge-pill ${badgeStyle}`}>
+          {persons.length}
+        </span>
+      </h5>
+    </div>
+    // )}
+    // </SimpleContext.Consumer>
   );
 };
 
